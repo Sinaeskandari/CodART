e4f37448306a626dcb3b8a021f9cceb75cfc12d1
Sina Eskandari <sinaeskandari007@gmail.com>
2021-02-06 15:28:19 +0330

    fixed some bugs and added some features for extract_interface

--- refactorings/extract_interface.py
+++ refactorings/extract_interface.py
@@ -37,11 +37,15 @@ class ExtractInterfaceListener(JavaParserLabeledListener):
     def exitMethodDeclaration(self, ctx: JavaParserLabeled.MethodDeclarationContext):
         method_name = ctx.IDENTIFIER().getText()
         method_type = ctx.typeTypeOrVoid().getText()
+        access = None
+        modifiers = ctx.parentCtx.parentCtx.modifier()
+        for m in modifiers:
+            access = access or m.classOrInterfaceModifier().PUBLIC() or m.classOrInterfaceModifier().PRIVATE() or m.classOrInterfaceModifier().PROTECTED()
         if self.is_enter_class:
             if method_name in self.method_params.keys():
-                self.interface_code += f'\t{method_type} {method_name}({", ".join(self.method_params[method_name])});\n'
+                self.interface_code += f'\t{access if access else ""} {method_type} {method_name}({", ".join(self.method_params[method_name])});\n'
             else:
-                self.interface_code += f'\t{method_type} {method_name}();\n'
+                self.interface_code += f'\t{access if access else ""} {method_type} {method_name}();\n'
 
     def exitClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
         if ctx.IDENTIFIER().getText() == self.class_name:
@@ -49,7 +53,6 @@ class ExtractInterfaceListener(JavaParserLabeledListener):
             self.interface_code += '}\n'
             self.token_stream_rewriter.insertBefore(program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
                                                     index=ctx.start.tokenIndex, text=self.interface_code)
-            self.token_stream_rewriter.replace(program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
-                                               from_idx=ctx.start.tokenIndex,
-                                               to_idx=ctx.start.tokenIndex + 1 + len(self.class_name),
-                                               text=f'class {self.class_name} implements {self.interface_name}')
+            self.token_stream_rewriter.insertAfter(program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
+                                                   index=ctx.classBody().start.tokenIndex - 1,
+                                                   text=f' implements {self.interface_name}')
